2308569907c938e83aee91a960b089501e16cae6
heyunfeng <heyunfeng@baidu.com>
2022-02-26 15:05:01 +0800

    修改vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-blog/',  
-  plugins: [vue()]
-})
+    base: '/vite-blog/',
+    server: {
+        host: '0.0.0.0'
+    },
+    plugins: [vue()]
+});
